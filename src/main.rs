78a284013475a28b7f7b95cb3b9c87029107b32a
hkattt <hugo.kat@outlook.com>
2024-05-21 03:19:45 +1000

    Documentation for main

--- src/main.rs
+++ src/main.rs
@@ -18,31 +18,32 @@ const TOPIC_RESULTS_FILE: &str  = "topic-results.csv";
 const SYS_RESULT_FILE: &str     = "sys-results.csv";
 
 // Publisher send duration (seconds)
-const SEND_DURATION: Duration   = Duration::from_secs(10); 
+const SEND_DURATION: Duration   = Duration::from_secs(1); 
 
-/**
- * TODO:
- * 1. Make the Analyser start EXACTLY when the publishers finish
- *      - Just make the publisher send everything to begin with?
- * 2. Make the Publisher actually end
- * 3. The program should go VERY quickly at 0ms delay and qos = 0
- * 4. Figure out why the first experiments literally receive nothing?
- */
-
+/// Entry point of the program. Spawns the analyser and publisher(s) needed to 
+/// conduct the experiments. 
+/// 
+/// Receives uses input according to the below usage:
+/// mqqt [-h <hostname>] [-p <port>] [-n <npublishers>] [-i <instancecount list>] [-q <qos list>] [-d <delay list>]
+/// where each list argument is a list of comma-seperated values.
+/// 
+/// # Returns
+/// A generic boxed error if any error is encountered.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Default program parameters
+    // 1 to 5 pubishers
+    // 3 different quality-of-service levels
+    // 0ms, 1ms 2ms, and 4ms delay
     let mut hostname = String::from("localhost");
     let mut port: u16 = 1883;
     let mut npublishers: u8 = 5;
-
-    // 1 to 5 publishers
     let mut instancecounts: Vec<u8> = vec![1, 2, 3, 4, 5];
-    // 3 different quality-of-service levels
     let mut qoss: Vec<QoS> = vec![QoS::AtMostOnce, QoS::AtLeastOnce, QoS::ExactlyOnce];
-    // 0ms, 1ms, 2ms, 4ms message delay
     let mut delays: Vec<u64> = vec![0, 1, 2, 4];
 
+    // Receives input arguments from the user 
+    // Skips the first argument since it is the program name
     let mut args_iter = env::args().skip(1);
     while let Some(arg) = args_iter.next() {
         match arg.as_str() {
@@ -123,14 +124,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         delays
     );
 
+    // Arc is used for hostname since the String is shared across 
+    // multiple threads
     let hostname = Arc::new(hostname);
+    
+    // Indicates whether the analyser is still conducting tests
+    // Used to tell the publishers to stop
     let running = Arc::new(Mutex::new(true));
 
+    // Counter values of all the publishers
+    // Used to show the 'actual' counter value to the analyser
     let counters: Vec<Arc<Mutex<u64>>> = (0..npublishers)
         .map(|_| Arc::new(Mutex::new(0)))
         .collect();
 
-    println!("Spawning publisher task(s)\n");
+    // Spawn publisher tasks
     let mut publisher_tasks = Vec::new();
     for publisher_index in 1..=npublishers {
         publisher_tasks.push(
@@ -146,7 +154,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         );
     }
 
-    println!("Spawning analyser task\n");
+    // Spawn analyser task
     let analyser_task = tokio::spawn(
         analyser::main_analyser(Arc::clone(&hostname), port, instancecounts, qoss, delays, counters)
     );
@@ -170,6 +178,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+/// Connects a client to the specified MQQT server
+/// 
+/// # Arguments
+/// * `client_id`: Unique ID of the client
+/// * `hostname`: Hostname of the MQQT server
+/// * `port`: Port that the MQQT server is on
+/// * `keep_alive`: Keep alive duration of the connection
+/// 
+/// # Returns
+/// Asynchronous client and event loop for the connection
 fn create_mqtt_conn(client_id: &str, hostname: &str, port: u16, keep_alive: Duration) -> (AsyncClient, EventLoop) {
     // Create MQTT options
     let mut options = MqttOptions::new(client_id, hostname, port);
@@ -180,6 +198,16 @@ fn create_mqtt_conn(client_id: &str, hostname: &str, port: u16, keep_alive: Dura
     AsyncClient::new(options, 10)
 }
 
+/// Subscribes a client to a given list of topics
+/// 
+/// # Arguments
+/// * `client`: Client to subscribe to the topics
+/// * `client_id`: Unique ID of the client
+/// * `qos`: Quality-of-service of the subscription
+/// * `topics`: Topics to subscribe to 
+/// 
+/// # Returns
+/// Nothing if successfull. `ClientError` if a subscription failed
 async fn subscribe_to_topics<S>(client: &AsyncClient, client_id: &str, qos: QoS, topics: &[S]) -> Result<(), ClientError> 
 where 
     S: AsRef<str> + Debug,
@@ -187,7 +215,6 @@ where
     for topic in topics {
         let topic_str = topic.as_ref();
         if let Err(error) = client.subscribe(topic_str, qos).await {
-            // TODO: Replace with debug prints
             debug_eprintln!("{} failed to subscribe to topic {} with error: {}", client_id, topic_str, error);
             return Err(error);
         } else {
@@ -198,10 +225,26 @@ where
     Ok(())
 }
 
+/// # Arguments
+/// * `instance`: Instance number of the publisher
+/// * `qos`: Quality-of-service of the publisher connection
+/// * `delay`: Message delay used by the publisher
+/// 
+/// # Returns
+/// The publisher topic string i.e. counter/<instance>/<qos>/<delay>
 fn publisher_topic_string(instance: u8, qos: QoS, delay: u64) -> String {
     format!("counter/{}/{}/{}", instance, qos_to_u8(qos), delay)
 }
 
+/// Returns the instance number in a publisher topic string
+/// 
+/// That is, returns <instance> from 
+/// 
+/// # Arguments
+/// `publisher_topic`: Publisher topic string: counter/<instance>/<qos>/<delay>
+/// 
+/// # Returns
+/// <instance> from the topic String if successful
 fn publisher_topic_instance(publisher_topic: &str) -> Option<usize> {
     let parts: Vec<&str> = publisher_topic.split('/').collect();
 
@@ -218,6 +261,11 @@ fn publisher_topic_instance(publisher_topic: &str) -> Option<usize> {
     }
 }
 
+/// # Arguments
+/// `qos`: Quality-of-service enum
+/// 
+/// # Returns
+/// Integer corresponding the the given QoS value
 fn qos_to_u8(qos: QoS) -> u8 {
     match qos {
         QoS::AtMostOnce  => 0, 
@@ -226,6 +274,11 @@ fn qos_to_u8(qos: QoS) -> u8 {
     }
 }
 
+/// # Arguments
+/// `qos`: Quality-of-service integer
+/// 
+/// # Returns
+/// Quality-of-service enum, if the integer is valid
 fn u8_to_qos(qos: u8) -> Option<QoS> {
     match qos {
         0 => Some(QoS::AtMostOnce),
@@ -235,6 +288,11 @@ fn u8_to_qos(qos: u8) -> Option<QoS> {
     }
 }
 
+/// # Arguments
+/// `qos`: Quality-of-service string
+/// 
+/// # Returns
+/// Quality-of-service enum, if the string is valid
 fn str_to_qos(qos: &str) -> Option<QoS> {
     match qos {
         "0" => Some(QoS::AtMostOnce),
@@ -244,6 +302,14 @@ fn str_to_qos(qos: &str) -> Option<QoS> {
     }
 }
 
+/// Converts a payload recevied from a MQQT broker to an integer
+/// Interprets the contents as BE bytes
+/// 
+/// # Arguments
+/// `publish`: Publish packet sent from a MQQT broker
+/// 
+/// # Returns
+/// Integer corresponding to the payload contents 
 fn bytes_to_u64(publish: &Publish) -> u64 {
     let payload = &publish.payload.to_vec();
     let mut array = [0u8; 8];
@@ -252,6 +318,14 @@ fn bytes_to_u64(publish: &Publish) -> u64 {
     u64::from_be_bytes(array)
 }
 
+/// Converts a payload recevied from a MQQT broker to an integer
+/// Interprets the contents as a utf8 string
+/// 
+/// # Arguments
+/// `publish`: Publish packet sent from a MQQT broker
+/// 
+/// # Returns
+/// Integer corresponding to the payload contents 
 fn utf8_to_u64(publish: &Publish) -> u64 {
     let payload = &publish.payload;
     let payload_str = std::str::from_utf8(&payload).unwrap();
